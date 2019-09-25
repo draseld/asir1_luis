9de7cbbdb141eee9ed96180b14e89d05c7338e76
draseld <luishijarrubia@hotmail.com>
2019-09-25 09:08:44 +0200

    php

--- asir1.php
+++ asir1.php
@@ -1,7 +1,7 @@
 Luis gomez hijarrubia (Alumno)
 <a href="http://192.168.0.113/asir1_victor/asir1.html">victor</a>
-<? php
-$ f = fopen ( ' visitas.txt ' , ' a ' );
-fwrite ( $ f , $ _SERVER [ ' REMOTE_ADDR ' ] . " \ r \ n " );
-fclose ( $ f )
-? >
+<?php
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fclose($f)
+?>
