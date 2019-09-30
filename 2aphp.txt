098e55ace06d7b5ba8708128fdf79cd9197a0f9c
draseld <luishijarrubia@hotmail.com>
2019-09-30 12:21:14 +0200

    txt

---
+++ 2aphp.txt
@@ -0,0 +1,14 @@
+
+<?php
+$a=5;
+$b=2;
+
+echo 'suma= '.($a+$b);
+echo '<br/>';
+echo 'resta= '.($a-$b);
+echo '<br/>';
+echo 'producto= '.($a*$b);
+echo '<br/>';
+echo 'division= '.($a/$b);
+
+?>
---
+++ catalogo.php
@@ -0,0 +1,21 @@
+Luis gomez hijarrubia (Alumno)
+<ul>
+	<li><a href="https://github.com/draseld/asir1_luis" target="_blank">Luis</a></li>
+	<li><a href="https://github.com/AaronASIR/Asir1_aaron" target="_blank">Aarón</a></li>
+	<li><a href="https://github.com/Real6/ASIR1_Ruben" target="_blank">Ruben</a></li>
+	<li><a href="https://github.com/victor-zayas/asir1_victorzayas" target="_blank">Victor</a></li>
+	<li><a href="https://github.com/deivisg98/asir1-david" target="_blank">David</a></li>
+	<li><a href="https://github.com/pablomoreta/asir1_pablo.git" target="_blank">Pablo</a></li>
+	<li><a href="https://github.com/adrivg19/asir1_adrian" target="_blank">Adrian</a></li>
+	<li><a href="https://github.com/MarioAlonso31info/asir1_mario" target="_blank">Mario</a></li>
+	<li><a href="https://github.com/bruno38pla/ASIR1_BRUNO" target="_blank">Bruno</a></li>
+	<li><a href="https://github.com/AlejandroGoVi/asir1_alex" target="_blank">Alex</a></li>
+	<li><a href="https://github.com/carlosccsan/asir1-cc" target="_blank">Carlos</a></li>
+	<li><a href="https://github.com/gargiljavi/Asir1_Javi" target="_blank">Javi</a></li>
+	<li><a href="https://github.com/sergiosamperio/asir1_sergio" target="_blank">Sergio</a></li>
+	<li><a href="https://github.com/Saioagil" target="_blank">Saioa</a></li>
+	<li><a href="https://github.com/alejandro-vgr/asir176" target="_blank">Alejandro</a></li>
+	<li><a href="https://github.com/serran24/asir1_victor" target="_blank">Victor</a></li>
+	<li><a href="https://github.com/kevin-raico-97" target="_blank">Kevin</a></li>
+	<li><a href="https://github.com/rolandoMH96/Asir1-RMH" target="_blank">Rolando</a></li>
+</ul>
