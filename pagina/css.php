eea640863e5fc8be9e846546f72df4480791a840
draseld <luishijarrubia@hotmail.com>
2019-11-11 12:16:41 +0100

    php

---
+++ pagina/css.php
@@ -0,0 +1,74 @@
+
+
+<html>
+<head>
+<title>Arthuria fate stay night</title>
+</head>
+<body bgcolor="blue">
+<img src="arthuria.jpeg" width=50%>
+<h1>Armas de Arthuria</h1>
+<h2><u>calibur</u></h2>
+<h3>Excalibur</h3>
+<h4>Rhongomyniad</h4>
+<h5>Carnwennan</h5>
+
+<center><font size=40; color=blue><p><b>Si quieres saber mas de fate say nigth asi como de otros proyectos de typemoon</b></p></font></center>
+<form action="fate.php" method="post">
+	Nombre: <input type="text" name="Nombre"></input> </br>
+	Apellidos: <input type="text" name="Apellidos"></input> </br>
+	Email: <input type="text" name="Email"></input> </br>
+	Direccion: <input type="text" name="Direccion"></input> </br>
+	Codigo Postal: <input type="text" name="CP"></input> </br>
+	Poblacion: <input type="text" name="Poblacion"></input> </br>
+	DNI: <input type="text" name="DNI"></input> </br>
+</form>
+<table style="width:50%; border:2px solid grey">
+  <tr>
+    <th style="border:2px solid grey">Autonomia</th>
+    <th style="border:1px solid grey">Provincia</th>
+  </tr>
+  <tr>
+    <td style="border:2px solid grey">Cantabria</td>
+    <td style="border:2px solid grey">Cantabria</td>
+  </tr>
+  <tr>
+    <td style="border:2px solid grey">Asturias</td>
+    <td style="border:2px solid grey">Asturia</td>
+  </tr>
+    <tr>
+    <td style="border:2px solid grey">Galicia</td>
+    <td style="border:2px solid grey">La Coruña</td>
+  </tr>
+    <tr>
+    <td style="border:2px solid grey"></td>
+    <td style="border:2px solid grey">Lugo</td>
+  </tr>
+	<td style="border:2px solid grey"></td>
+	<td style="border:2px solid grey">Pontevedra</td>
+  </tr>
+    <tr>
+    <td style="border:2px solid grey"></td>
+    <td style="border:2px solid grey">Ourense</td>
+  </tr>
+    <tr>
+    <td style="border:2px solid grey">Pais Vasco</td>
+    <td style="border:2px solid grey">Vizcaya</td>
+  </tr>
+    <tr>
+    <td style="border:2px solid grey"></td>
+    <td style="border:2px solid grey">Guipuzcoa</td>
+  </tr>
+  <tr>
+    <td style="border:2px solid grey"></td>
+    <td style="border:2px solid grey">Alava</td>
+  </tr>
+</table>
+	
+<img src="excalibur.gif" style="width:60%">
+
+</body>
+
+
+
+
+
---
+++ pagina/html.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<title>Arthuria fate stay night</title>
+</head>
+<body bgcolor="blue">
+<img src="arthuria.jpeg" width=10%>
+<h1>Armas de Arthuria</h1>
+<h2><u>calibur</u></h2>
+<h3>Excalibur</h3>
+<h4>Rhongomyniad</h4>
+<h5>Carnwennan</h5>
+
+<center><font size=40; color=blue><p><b>Si quieres saber mas de fate say nigth asi como de otros proyectos de typemoon</b></p></font></center>
+<form action="fate.php" method="post">
+	Nombre: <input type="text" name="Nombre"></input> </br>
+	Apellidos: <input type="text" name="Apellidos"></input> </br>
+	Email: <input type="text" name="Email"></input> </br>
+	Direccion: <input type="text" name="Direccion"></input> </br>
+	Codigo Postal: <input type="text" name="CP"></input> </br>
+	Poblacion: <input type="text" name="Poblacion"></input> </br>
+	DNI: <input type="text" name="DNI"></input> </br>
+	
+</form>
+
+<table width="50%" border="1">
+<table width="50%" border="1">
+  <tr>
+    <th>Autonomia</th>
+    <th>Provincia</th>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+    <td>Cantabria</td>
+  </tr>
+  <tr>
+    <td>Asturias</td>
+    <td>Asturia</td>
+  </tr>
+    <tr>
+    <td>Galicia</td>
+    <td>La Coruña</td>
+  </tr>
+    <tr>
+    <td></td>
+    <td>Lugo</td>
+  </tr>
+	<td></td>
+	<td>Pontevedra</td>
+  </tr>
+    <tr>
+    <td></td>
+    <td>Ourense</td>
+  </tr>
+    <tr>
+    <td>Pais Vasco</td>
+    <td>Vizcaya</td>
+  </tr>
+    <tr>
+    <td></td>
+    <td>Guipuzcoa</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Alava</td>
+  </tr>
+</table>
+	
+<img src="excalibur.gif" width=60%>
+
+</body>
+
+
